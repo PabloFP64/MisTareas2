e70597dd06ce834809182f7711e8d55e5aa6fc98
PabloFP64 <PabloFP64@gmail.com>
2022-11-04 22:39:38 +0100

    Boton registrar devuelve al pricipal pero sin hacer nada

--- app/src/main/java/com/example/mistareas2/CrearTarea.java
+++ app/src/main/java/com/example/mistareas2/CrearTarea.java
@@ -19,8 +19,8 @@ import com.example.mistareas2.task_database.TaskDatabaseHelper;
 import java.util.ArrayList;
 
 public class CrearTarea extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE ="com.example.android.mistareas2.extra.MESSAGE";
-    private EditText mMessageEditText;
+    public static final String EXTRA_REPLY ="com.example.android.mistareas2.extra.MESSAGE";
+    private EditText mNombre;
     private EditText mReply;
 
     private TaskDatabaseHelper taskHelper;
@@ -32,86 +32,64 @@ public class CrearTarea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_tarea);
+        mNombre = findViewById(R.id.Texto_Nombre);
+        Intent intent = getIntent();
 
 
-        String task = String.valueOf(mReply.getText());
-        SQLiteDatabase db = taskHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
-        db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-        db.close();
-        updateUI();
+
+//        String task = String.valueOf(mReply.getText());
+//        SQLiteDatabase db = taskHelper.getWritableDatabase();
+//        ContentValues values = new ContentValues();
+//        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+//        db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+//        db.close();
+//        updateUI();
 
 
     }
-    //    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_add_task:
-//                final EditText taskEdit = new EditText(this);
-//                AlertDialog dialog = new AlertDialog.Builder(this)
-//                        .setTitle("Add a new task").setMessage("What do you want to do next?").setView(taskEdit)
-//                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                String task = String.valueOf(taskEdit.getText());
-//                                SQLiteDatabase db = taskHelper.getWritableDatabase();
-//                                ContentValues values = new ContentValues();
-//                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
-//                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-//                                db.close();
-//                                updateUI();
-//                            }
-//                        })
-//                        .setNegativeButton("Cancel", null).create();
-//                dialog.show();
-//                return true;
-//
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
 
     public void launchTareaCreada(View view) {
-        Intent intent = new Intent(this, CrearTarea.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-
-
-
-    }
-
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
+        String nombre = mNombre.getText().toString();
         Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_MESSAGE, reply);
         setResult(RESULT_OK,replyIntent);
         finish();
 
 
+
     }
-
-    private void updateUI() {
-        ArrayList<String> taskList = new ArrayList<>();
-        SQLiteDatabase db = taskHelper.getReadableDatabase();
-        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-                null, null, null, null, null);
-        while (cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-            taskList.add(cursor.getString(idx));
-        }
-
-        if (arrAdapter == null) {
-            arrAdapter = new ArrayAdapter<>(this, R.layout.todo_task, R.id.title_task, taskList);
-            TaskList.setAdapter(arrAdapter);
-        } else {
-            arrAdapter.clear();
-            arrAdapter.addAll(taskList);
-            arrAdapter.notifyDataSetChanged();
-        }
-
-        cursor.close();
-        db.close();
-    }
+//
+//    public void returnReply(View view) {
+//        String reply = mReply.getText().toString();
+//        Intent replyIntent = new Intent();
+//        replyIntent.putExtra(EXTRA_MESSAGE, reply);
+//        setResult(RESULT_OK,replyIntent);
+//        finish();
+//
+//
+//    }
+//
+//    private void updateUI() {
+//        ArrayList<String> taskList = new ArrayList<>();
+//        SQLiteDatabase db = taskHelper.getReadableDatabase();
+//        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+//                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+//                null, null, null, null, null);
+//        while (cursor.moveToNext()) {
+//            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+//            taskList.add(cursor.getString(idx));
+//        }
+//
+//        if (arrAdapter == null) {
+//            arrAdapter = new ArrayAdapter<>(this, R.layout.todo_task, R.id.title_task, taskList);
+//            TaskList.setAdapter(arrAdapter);
+//        } else {
+//            arrAdapter.clear();
+//            arrAdapter.addAll(taskList);
+//            arrAdapter.notifyDataSetChanged();
+//        }
+//
+//        cursor.close();
+//        db.close();
+//    }
 
 }
